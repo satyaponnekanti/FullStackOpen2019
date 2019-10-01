995029f312b4a88bf3525b8ea6718e13cec2bc4b
glgayathri <glgayathri@gmail.com>
2019-10-01 12:20:41 +0200

    React HW1

--- Part1/CourseInfo/courseinfo/src/index.js
+++ Part1/CourseInfo/courseinfo/src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const coursename = 'Half Stack application development'
+const part1 = 'Fundamentals of React'
+const exercises1 = 10
+const part2 = 'Using props to pass data'
+const exercises2 = 7
+const part3 = 'State of a component'
+const exercises3 = 14
+const totalexcerices=exercises1+exercises2+exercises3;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header = (props) => {
+    return (
+      <>
+          <h1>{props.course}</h1>
+      </>
+    )
+  }
+
+  const Content = (props) => {
+    return (
+      <>
+      <p>{props.partname}: {props.exercisesno}</p>
+      </>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+        <>
+      Number of exercises: {props.total}
+      </>
+    )
+  }
+
+
+const App = () => {
+  return (
+    <>
+      <Header course={coursename}/>
+      <Content partname={part1} exercisesno={exercises1} />
+      <Content partname={part2} exercisesno={exercises2} />
+      <Content partname={part3} exercisesno={exercises3} />
+      <Total total={totalexcerices}/>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
