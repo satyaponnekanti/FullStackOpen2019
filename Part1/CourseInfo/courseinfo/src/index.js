5a0225251371ea45bd6253e36038510f6baf3f50
glgayathri <glgayathri@gmail.com>
2019-10-02 14:22:41 +0200

    Part1_Exercise:1.3_added

--- Part1/CourseInfo/courseinfo/src/index.js
+++ Part1/CourseInfo/courseinfo/src/index.js
@@ -38,6 +38,19 @@ const Header = (props) => {
 
 
 const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
   return (
     <>
       <Header course={coursename}/>
